d11cefef982c86c13a4dbfa8dbf92ed259d66419
Yavanni <yavanni@Yavannis-MacBook-Pro.local>
2023-10-03 12:13:43 -0400

    fix: swapped prod and nightly

--- compose.sh
+++ compose.sh
@@ -1,6 +1,6 @@
 echo "Build API"
-ARCH=${ARCH-64} docker compose -f dockers/api/docker-compose.yaml build
+ARCH=${ARCH-64} docker compose -f dockers/api/docker-compose.yaml up --build --detach
 echo "Build Nightly"
-docker compose -f dockers/nightly-website/docker-compose.yaml build
+docker compose -f dockers/nightly-website/docker-compose.yaml up --build --detach
 echo "Build Production"
-docker compose -f dockers/prod-website/docker-compose.yaml build
+docker compose -f dockers/prod-website/docker-compose.yaml up --build --detach
